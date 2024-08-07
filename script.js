6c94dd30a455fc6654c82eec8abfc3ad9ba84191
Mostafa Abdelglel <oomaooka09@gmail.com>
2024-08-07 22:52:55 +0300

    change algorithm
    change algo to function operation that takes string
    contains bugs but works fine in most cases

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container");
-const screen = document.querySelector(".screen");
-let isFirst = false;
-let first = "";
-let second = "";
-let operator;
+const inputScreen = document.querySelector(".inputScreen");
+const resultScreen = document.querySelector(".resultScreen");
+let isFirstOperator = true;
+let onScreen = "";
+let expression = "";
 
 function add (num1 , num2){
     return num1+num2;
@@ -17,49 +17,100 @@ function multiply(num1,num2){
 function division(num1,num2){
     return num1/num2;
 }
-function operation(num1,num2,operator){
+function isOperator(character){
+    return (character==='+' || character==='-' || character==='*' || character==='/');
+}
+function operation(str){
+    const arr = str.split("");
+    let num1 = "" , num2 = "" ,operator;
+    let num1Finished = false;
+    for(let i = 0 ; i < arr.length ;i++){
+        if(isOperator(arr[i]) && i!==0){
+            operator=arr[i];
+            num1Finished = true;
+        }
+        else if(num1Finished)
+            num2+=arr[i];
+        else
+            num1+=arr[i];
+    }
     switch(operator){
         case '+':
-            return add(num1,num2);
+            return add(+num1,+num2);
         case '-':
-            return subtract(num1,num2);
+            return subtract(+num1,+num2);
         case '*':
-            return multiply(num1,num2);
+            return multiply(+num1,+num2);
         case '/':
-            return division(num1,num2);
+            return division(+num1,+num2);
+        default :
+            if(num1.length===0)
+                return "ERROR"
+            else
+                return num1;
     }
 }
 
 container.addEventListener('click',(e)=>{
     switch(e.target.className){
         case "digit" :
-            if(!isFirst)
-                first+=e.target.textContent;
-            else
-                second+=e.target.textContent;
+            if(expression.slice(-1)==='='){
+                onScreen=expression=e.target.textContent;
+            }
+            else{
+                onScreen+=e.target.textContent;
+                expression+=e.target.textContent;
+            }
             break;
         case "operator" :
-            if(!isFirst)
-                isFirst = true;
-            else
-                operation(first,second,operator);
-            operator = e.target.textContent;
+            if(isOperator(onScreen.slice(-1))){
+                onScreen = onScreen.slice(0,-1)+e.target.textContent;
+                expression= expression.slice(0,-1)+e.target.textContent;
+            }
+            else if(isFirstOperator){
+                isFirstOperator = false;
+                onScreen+=e.target.textContent;
+                expression+=e.target.textContent;
+            }
+            else if(expression.slice(-1)==='='){
+                expression = expression.slice(0,-1);
+                expression+=e.target.textContent;
+                onScreen=expression;
+            }
+            else{
+                resultScreen.textContent = expression=operation(expression).toString();
+                onScreen+=e.target.textContent;
+                expression+=e.target.textContent;
+            }
             break;
         case "action":
             switch(e.target.id){
                 case "equal":
-                    operation(first,second,operator);
+                    if(expression.slice(-1)!=='='){
+                        resultScreen.textContent=expression=operation(expression).toString();
+                        onScreen=expression;
+                        expression+=e.target.textContent;
+                    }
                     break;
                 case "backspace":
-                    if(!isFirst)
-                        first = first.slice(0,-1);
-                    else
-                        second = second.slice(0,-1);
+                    if(onScreen!==0){
+                        onScreen = onScreen.slice(0,-1);
+                        if(expression.slice(-1)==='=')
+                            expression = expression.slice(0,-2);
+                        else
+                            expression=expression.slice(0,-1);
+                        if(isOperator(expression.slice(-1)))
+                            isFirstOperator=true;
+                    }
                     break;
                 case "clear":
-                    first = second = "";
-                    isFirst = false;
+                    onScreen = '';
+                    expression="";
+                    isFirstOperator= true;
+                    resultScreen.textContent = '0';
             }
+            break;
     }
+    inputScreen.textContent = onScreen;
 
 });
